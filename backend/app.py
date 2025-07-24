060f12a6c1aebd03fba7e5fa8454a5c3dad89ea4
wildc4rd23 <wildc4rd.23@googlemail.com>
2025-07-24 16:21:19 +0200

    port change, debug on

--- backend/app.py
+++ backend/app.py
@@ -367,5 +367,5 @@ def catch_all(path):
 
 if __name__ == '__main__':
     # Für Render deployment optimiert
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
